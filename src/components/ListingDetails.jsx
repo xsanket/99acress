43eba9d1c0134522141bb3f64a3b4b52240a2ef3
xsanket <xsanketkamble232@gmail.com>
2024-03-21 15:18:32 +0530

    "shareCopy functionality"

--- src/components/ListingDetails.jsx
+++ src/components/ListingDetails.jsx
@@ -8,11 +8,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay, Navigation, Pagination } from 'swiper/modules';
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkedCopied, setShareLinkedCopied] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
     async function fetchListing() {
@@ -50,6 +52,42 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
-    </main>
+
+      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2
+       border-gray-400 
+      rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkedCopied(true);
+          setTimeout(() => {
+            setShareLinkedCopied(false)
+          }, 2000);
+        }} >
+
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkedCopied && (
+        <div className="fixed top-[23%] right-[5%] z-10">
+          <div className="flex items-center border-2 border-gray-400 rounded-md bg-white p-2 font-semibold">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2 text-green-500"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 1a9 9 0 1 1 0 18A9 9 0 0 1 10 1zm4.95 6.707a1 1 0 0 0-1.414-1.414L10 11.586l-1.536-1.536a1 1 0 1 0-1.414 1.414l2.25 2.25a1 1 0 0 0 1.414 0l4-4z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Link Copied
+          </div>
+        </div>
+      )}
+
+
+
+    </main >
   );
 }
