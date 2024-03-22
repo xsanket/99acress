8bede6ac0234c17add84bdf191c60b0f3f46a667
xsanket <xsanketkamble232@gmail.com>
2024-03-22 16:17:58 +0530

    "Added map functionality"

--- src/components/ListingDetails.jsx
+++ src/components/ListingDetails.jsx
@@ -14,6 +14,7 @@ import { FaBed } from "react-icons/fa";
 import { FaParking, FaChair, FaBath } from "react-icons/fa";
 import { getAuth } from "firebase/auth"
 import Contact from "./Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 
 export default function Listing() {
@@ -158,11 +159,32 @@ export default function Listing() {
             </div>
           )}
           {contactLandlord && (
-            <Contact userRef ={listing.userRef} listing={listing} />
+            <Contact userRef={listing.userRef} listing={listing} />
           )}
 
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+
+        {/* map div */}
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main >
   );
